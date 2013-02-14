335ba96d77175bdd99b89fbbb9d68ed26c8dc2c5
alecxe <afanasieffav@gmail.com>
2013-02-14 14:42:36 +0400

    Added livingsocial.com scrapy project with pipelining to the database.

---
+++ scrape/lib/full_source/tutorial/tutorial/models.py
@@ -0,0 +1,44 @@
+#! -*- coding: utf-8 -*-
+
+"""
+Web Scraper Project
+
+Scrape data from a regularly updated website livingsocial.com and
+save to a database (postgres).
+
+Database models part - defines table for storing scraped data.
+Direct run will create the table.
+"""
+
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from  sqlalchemy.engine.url import URL
+import settings
+
+
+DeclarativeBase = declarative_base()
+
+def db_connect():
+    """
+    Performs database connection using database settings from settings.py.
+    Returns sqlalchemy engine instance
+    """
+    return create_engine(URL(**settings.DATABASE))
+
+def create_deals_table(engine):
+    """"""
+    DeclarativeBase.metadata.create_all(engine)
+
+
+class Deals(DeclarativeBase):
+    """Sqlalchemy deals model"""
+    __tablename__ = "deals"
+
+    id = Column(Integer, primary_key=True)
+    title = Column('title', String)
+    description = Column('description', String, nullable=True)
+    link = Column('link', String, nullable=True)
+    location = Column('location', String, nullable=True)
+    category = Column('category', String, nullable=True)
+    original_price = Column('original_price', String, nullable=True)
+    price = Column('price', String, nullable=True)
