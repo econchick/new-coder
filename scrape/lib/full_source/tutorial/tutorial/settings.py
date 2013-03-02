ec30ccc8dd803ddbf6e871f8d08b491de01e7028
Lynn Root <erin.lynn.root@gmail.com>
2013-03-01 16:22:04 -0800

    blarg

---
+++ scrape/lib/full_source/tutorial/tutorial/settings.py
@@ -0,0 +1,24 @@
+# Scrapy settings for tutorial project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'tutorial'
+
+SPIDER_MODULES = ['tutorial.spiders']
+NEWSPIDER_MODULE = 'tutorial.spiders'
+
+ITEM_PIPELINES = ['tutorial.pipelines.LivingSocialPipeline']
+
+DATABASE = {'drivername': 'postgres',
+            'host': 'localhost',
+            'port': '5432',
+            'username': 'lynnroot',
+            'password': 'root',
+            'database': 'scrape'}
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+# USER_AGENT = 'tutorial (+http://www.yourdomain.com)'
