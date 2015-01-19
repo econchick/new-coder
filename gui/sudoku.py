567ea250ffc3fc44c7744e4e6d4986299e269314
Lynn Root <lynn@spotify.com>
2015-01-18 16:16:33 -0800

    WIP: writing of gui tutorial

--- gui/sudoku.py
+++ gui/sudoku.py
@@ -1,11 +1,8 @@
 import argparse
-import sys
-import random
 
 from Tkinter import Tk, Canvas, Frame, Button, BOTH, TOP, BOTTOM
 
-DEBUG = True  # Exceptions are raised for debugging purposes
-LEVELS = ['debug', 'n00b', 'l33t']  # Available levels
+BOARDS = ['debug', 'n00b', 'l33t', 'error']  # Available sudoku boards
 MARGIN = 20  # Pixels around the board
 SIDE = 50  # Width of every board cell.
 WIDTH = HEIGHT = MARGIN * 2 + SIDE * 9  # Width and height of the whole board
@@ -18,39 +15,22 @@ class SudokuError(Exception):
     pass
 
 
-def check_negative(value):
-    """
-    Checks if value is a positive integer.
-    Throws ArgumentTypeError if not.
-    """
-    ivalue = int(value)
-    if ivalue < 0:
-        raise argparse.ArgumentTypeError("%s is an invalid positive int value"
-                                         % value)
-    return ivalue
-
-
 def parse_arguments():
     """
     Parses arguments of the form:
-        sudoku.py <level name> [board number]
-    Where `level name` must be in the LEVELS list and `board number` must be
-    a positive integer.
+        sudoku.py <board name>
+    Where `board name` must be in the `BOARD` list
     """
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("--level",
+    arg_parser.add_argument("--board",
                             help="Level name.",
                             type=str,
-                            choices=LEVELS,
+                            choices=BOARDS,
                             required=True)
-    arg_parser.add_argument("--board",
-                            help="Board number. Must be a positive integer.",
-                            type=check_negative,
-                            default=-1)
 
     # Returns a dictionary of keys = argument flag, and value = argument
     args = vars(arg_parser.parse_args())
-    return args['level'], args['board']
+    return args['board']
 
 
 class SudokuUI(Frame):
@@ -62,22 +42,20 @@ class SudokuUI(Frame):
         Frame.__init__(self, parent)
         self.parent = parent
 
-        self.row, self.col = -1, -1
+        self.row, self.col = 0, 0
 
         self.__initUI()
 
     def __initUI(self):
         self.parent.title("Sudoku")
-        self.pack(fill=BOTH, expand=1)
-        self.canvas = Canvas(
-            self, width=WIDTH, height=HEIGHT,
-            highlightthickness=0
-        )
+        self.pack(fill=BOTH)
+        self.canvas = Canvas(self,
+                             width=WIDTH,
+                             height=HEIGHT)
         self.canvas.pack(fill=BOTH, side=TOP)
-        clear_button = Button(
-            self, text="Clear answers",
-            command=self.__clear_answers
-        )
+        clear_button = Button(self,
+                              text="Clear answers",
+                              command=self.__clear_answers)
         clear_button.pack(fill=BOTH, side=BOTTOM)
 
         self.__draw_grid()
@@ -88,7 +66,7 @@ class SudokuUI(Frame):
 
     def __draw_grid(self):
         """
-        Draws grid divided with blue lines into squares 3x3
+        Draws grid divided with blue lines into 3x3 squares
         """
         for i in xrange(10):
             color = "blue" if i % 3 == 0 else "gray"
@@ -109,12 +87,12 @@ class SudokuUI(Frame):
         self.canvas.delete("numbers")
         for i in xrange(9):
             for j in xrange(9):
-                answer = self.game.answer[i][j]
+                answer = self.game.puzzle[i][j]
                 if answer != 0:
                     x = MARGIN + j * SIDE + SIDE / 2
                     y = MARGIN + i * SIDE + SIDE / 2
-                    original = self.game.puzzle[i][j]
-                    color = "black" if answer == original else "slate gray"
+                    original = self.game.start_puzzle[i][j]
+                    color = "black" if answer == original else "sea green"
                     self.canvas.create_text(
                         x, y, text=answer, tags="numbers", fill=color
                     )
@@ -151,8 +129,7 @@ class SudokuUI(Frame):
         if self.game.game_over:
             return
         x, y = event.x, event.y
-        if (MARGIN < x < WIDTH - MARGIN and
-            MARGIN < y < HEIGHT - MARGIN):
+        if (MARGIN < x < WIDTH - MARGIN and MARGIN < y < HEIGHT - MARGIN):
             self.canvas.focus_set()
 
             # get row and col numbers from x,y coordinates
@@ -172,7 +149,7 @@ class SudokuUI(Frame):
         if self.game.game_over:
             return
         if self.row >= 0 and self.col >= 0 and event.char in "1234567890":
-            self.game.answer[self.row][self.col] = int(event.char)
+            self.game.puzzle[self.row][self.col] = int(event.char)
             self.col, self.row = -1, -1
             self.__draw_puzzle()
             self.__draw_cursor()
@@ -180,73 +157,56 @@ class SudokuUI(Frame):
                 self.__draw_victory()
 
     def __clear_answers(self):
-        self.game.set_answer_to_puzzle()
+        self.game.start()
         self.canvas.delete("victory")
         self.__draw_puzzle()
 
 
 class SudokuBoard(object):
     """
-    Simple class for initializing sudoku board
+    Sudoku Board representation
     """
-    def __init__(self, boards_file):
-        self.boards = [[]]
-        for line in boards_file:
+    def __init__(self, board_file):
+        self.board = self.__create_board(board_file)
+
+    def __create_board(self, board_file):
+        board = []
+        for line in board_file:
             line = line.strip()
             if len(line) != 9:
-                self.boards = []
                 raise SudokuError(
                     "Each line in the sudoku puzzle must be 9 chars long."
                 )
-            if len(self.boards[-1]) == 9:
-                self.boards.append([])
-
-            self.boards[-1].append([])
+            board.append([])
 
             for c in line:
-                if c not in "1234567890":
+                if not c.isdigit():
                     raise SudokuError(
                         "Valid characters for a sudoku puzzle must be in 0-9"
                     )
-                self.boards[-1][-1].append(int(c))
+                board[-1].append(int(c))
 
-        if len(self.boards[-1]) != 9:
-            self.boards = []
-            raise SudokuError(
-                "Each sudoku puzzle must be 9 lines long"
-            )
-
-    def get_boards(self):
-        return self.boards
+        if len(board) != 9:
+            raise SudokuError("Each sudoku puzzle must be 9 lines long")
+        return board
 
 
 class SudokuGame(object):
     """
     A Sudoku game, in charge of storing the state of the board and checking
-    wether the puzzle is completed.
+    whether the puzzle is completed.
     """
-    def __init__(self, boards_file):
-        self.boards_file = boards_file
-        board = SudokuBoard(boards_file)
-        self.boards = board.get_boards()
+    def __init__(self, board_file):
+        self.board_file = board_file
+        self.start_puzzle = SudokuBoard(board_file).board
 
-    def start(self, board_number):
-        if board_number == -1:
-            board_number = random.randrange(len(self.boards))
-        elif board_number < 0 or board_number >= len(self.boards):
-            raise SudokuError(
-                "Can't find board number %d" % board_number
-            )
-        self.puzzle = self.boards[board_number]
-        self.set_answer_to_puzzle()
-
-    def set_answer_to_puzzle(self):
+    def start(self):
         self.game_over = False
-        self.answer = []
+        self.puzzle = []
         for i in xrange(9):
-            self.answer.append([])
+            self.puzzle.append([])
             for j in xrange(9):
-                self.answer[i].append(self.puzzle[i][j])
+                self.puzzle[i].append(self.start_puzzle[i][j])
 
     def check_win(self):
         for row in xrange(9):
@@ -266,17 +226,17 @@ class SudokuGame(object):
         return set(block) == set(range(1, 10))
 
     def __check_row(self, row):
-        return self.__check_block(self.answer[row])
+        return self.__check_block(self.puzzle[row])
 
     def __check_column(self, column):
         return self.__check_block(
-            [self.answer[row][column] for row in xrange(9)]
+            [self.puzzle[row][column] for row in xrange(9)]
         )
 
     def __check_square(self, row, column):
         return self.__check_block(
             [
-                self.answer[r][c]
+                self.puzzle[r][c]
                 for r in xrange(row * 3, (row + 1) * 3)
                 for c in xrange(column * 3, (column + 1) * 3)
             ]
@@ -284,20 +244,13 @@ class SudokuGame(object):
 
 
 if __name__ == '__main__':
-    level_name, board_number = parse_arguments()
+    board_name = parse_arguments()
 
-    try:
-        with open('%s.sudoku' % level_name, 'r') as boards_file:
-            game = SudokuGame(boards_file)
-            game.start(board_number)
+    with open('%s.sudoku' % board_name, 'r') as boards_file:
+        game = SudokuGame(boards_file)
+        game.start()
 
-            root = Tk()
-            ui = SudokuUI(root, game)
-            root.geometry("%dx%d" % (WIDTH, HEIGHT + 40))
-            root.mainloop()
-    except SudokuError, e:
-        print "Puzzles file is invalid."
-        if DEBUG:
-            raise e
-        else:
-            sys.exit(1)
+        root = Tk()
+        ui = SudokuUI(root, game)
+        root.geometry("%dx%d" % (WIDTH, HEIGHT + 40))
+        root.mainloop()
